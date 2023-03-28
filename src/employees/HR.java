fd170594ca1048f66873ec3c4eb4493f8ad45166
Yuriy S <dev@solovyov.de>
2023-03-28 11:42:30 +0200

    add hr hire function; add new class Recruter and rendom names

--- src/employees/HR.java
+++ src/employees/HR.java
@@ -1,13 +1,46 @@
 package employees;
 
 public class HR extends Employee {
+    public enum EmployeeType {
+        ARCHITECT,
+        BUYER,
+        WORKER,
+        WORKER_MANAGER
+    }
     public HR(String firstname, String lastname)
             throws NullPointerException {
         super(firstname, lastname);
     }
 
-    public boolean hireEmployee(Employee e, int amount) {
-        return false;
+    /**
+     * Hires employee of the given type based on first and last names.
+     * @param eType EmployeeType, enum value
+     * @param firstname String first name of the person to hire
+     * @param lastname String last name of the person to hire
+     * @return new employee based on type
+     * @throws NullPointerException if the employee type not valid
+     */
+    public Employee hireEmployee(
+            EmployeeType eType, String firstname, String lastname) throws NullPointerException {
+        Employee newEmployee = null;
+        switch(eType) {
+            case ARCHITECT -> {
+                newEmployee = new Architect(firstname, lastname);
+            }
+            case BUYER -> {
+                newEmployee = new Buyer(firstname, lastname);
+            }
+            case WORKER -> {
+                newEmployee = new Worker(firstname, lastname);
+            }
+            case WORKER_MANAGER -> {
+                newEmployee = new WorkerManager(firstname, lastname);
+            }
+            default -> {
+                throw new NullPointerException("ERROR: Employee type is not valid!");
+            }
+        }
+        return newEmployee;
     }
 
     /*
